6f88fe1f33236009ad7af6eb103ec26ed7bc07ee
Apple <darshan@apples-macbook-pro.local>
2019-03-09 13:07:36 +0530

    1. Project Setup 2. Pod install with Alamofire framework to call Api's.

---
+++ Pods/Target Support Files/Pods-TableListDemoTests/Pods-TableListDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TableListDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_TableListDemoTests
+@end
---
+++ TableListDemo/Views/UITableCells/ListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListViewCell.swift
+//  TableListDemo
+//
+//  Created by Darshan on 09/03/19.
+//  Copyright © 2019 Darshan. All rights reserved.
+//
+
+import UIKit
+
+class ListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
